142249081be4c4a4549161376d4c705d9ecee3bf
Lauren Posey <mrslaurenposey@gmail.com>
2016-06-03 10:03:30 -0700

    add html and some frontend and backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
+// backend
+
+var peruCount = 0;
+var greeceCount = 0;
+var indiaCount = 0;
+
+var evalQuestion = function (res) {
+  if (res==="greece") greeceCount++;
+  if (res==="peru") peruCount++;
+  if (res==="india") indiaCount++;
+}
+
+// frontend
+
 $(document).ready(function(){
   $("form#vacaSurvey").submit(function(event){
     event.preventDefault();
+    evalQuestion($("#animal").val());
+    evalQuestion($("#activity").val());
+    evalQuestion($("#personality").val());
+    evalQuestion($("#food").val());
+    evalQuestion($("#fabric").val());
+
     
   });
 });
